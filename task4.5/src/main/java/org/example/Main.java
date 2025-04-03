5efb55375d346d2cc049bf54165e11c7338d2a4d
BMV17 <MMV1708@yandex.ru>
2025-04-03 18:53:30 +0300

    Задача 4.5

---
+++ task4.5/src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import java.util.stream.IntStream;
+
+public class Main {
+    public static void main(String[] args) {
+        long sum = IntStream.rangeClosed(10000000, 1000000000).filter(i -> i < 1000000000).sum();
+        System.out.println(sum);
+    }
+}
