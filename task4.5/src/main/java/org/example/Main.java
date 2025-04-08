e3f9083d4ba175cf5c377bf645d842a00d7b1ee6
BMV17 <MMV1708@yandex.ru>
2025-04-08 20:33:41 +0300

    Задача 4.5 Надеюсь на Божью помощь

--- task4.5/src/main/java/org/example/Main.java
+++ task4.5/src/main/java/org/example/Main.java
@@ -4,7 +4,8 @@ import java.util.stream.IntStream;
 
 public class Main {
     public static void main(String[] args) {
-        long sum = IntStream.rangeClosed(10000000, 1000000000).filter(i -> i < 1000000000).sum();
+        long sum = LongStream.range(10000000,1000000000).sum();
+       /// long sum = IntStream.rangeClosed(10000000, 1000000000).filter(i -> i < 1000000000).sum();
         System.out.println(sum);
     }
 }
